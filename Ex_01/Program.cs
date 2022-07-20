df48eca8ae6eacf0cc2f92f210ce83edd234edb4
o.gudkova <oegshop@gmail.com>
2022-07-20 18:46:55 +0400

    Ex_01

---
+++ Ex_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+if (A/B==B)
+{
+    Console.WriteLine($"{A} number square {B}");
+}
+else
+{
+    Console.WriteLine($"{A} not a square number {B}");
+}
