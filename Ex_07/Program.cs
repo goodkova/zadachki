21b020210831e3a67a64f3839ef30bc1e54ffa18
o.gudkova <oegshop@gmail.com>
2022-07-21 22:23:29 +0400

    Ex_07

---
+++ Ex_07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать числа от -N до N
+
+Console.Clear();
+int N = new Random().Next(-10,10);
+if (N < 0)
+{
+    N = -N;
+}
+for (int i = -N; i <= N; i++)
+{
+    Console.Write($"{i}, ");
+}
+
+
