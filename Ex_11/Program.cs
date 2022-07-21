72c0e1f4236c757abda565bcee188f00dd4f23cb
o.gudkova <oegshop@gmail.com>
2022-07-21 23:03:15 +0400

    Ex_11

---
+++ Ex_11/Program.cs
@@ -0,0 +1,17 @@
+﻿// 11. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.Clear();
+int N = new Random().Next(10, 100);
+Console.WriteLine($"N = {N}");
+int units = N % 10;
+int dozens = N / 10;
+
+if (units > dozens)
+{
+    Console.WriteLine($"Result = {units}");
+}
+else
+{
+    Console.WriteLine($"Result = {dozens}");
+}
+    
