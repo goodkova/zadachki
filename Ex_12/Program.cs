d4c026dcb9046f562aeffe31075d0f3bb48c1d13
o.gudkova <oegshop@gmail.com>
2022-07-21 23:21:33 +0400

    Ex_12

---
+++ Ex_12/Program.cs
@@ -0,0 +1,9 @@
+﻿// 12. Удалить вторую цифру трёхзначного числа
+
+Console.Clear();
+int N = new Random().Next(100, 1000);
+Console.WriteLine($"N = {N}");
+int units = N % 10;
+int hundreds = N / 100;
+int result = hundreds * 10 + units;
+Console.WriteLine($"Result = {result}");
