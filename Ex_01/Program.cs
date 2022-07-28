e4b5a8077f5c752ae7851cfcdf4de62b3ae4d766
o.gudkova <oegshop@gmail.com>
2022-07-28 14:05:03 +0400

    Ex_15

--- Ex_01/Program.cs
+++ Ex_01/Program.cs
@@ -1,9 +1,9 @@
 ﻿// По двум заданным числам проверять является ли первое квадратом второго
 
-int A = new Random().Next(-10,10);
-int B = new Random().Next(-10,10);
-// int A = 3;
-// int B = 9;
+// int A = new Random().Next(-10,10);
+// int B = new Random().Next(-10,10);
+double A = Double.PositiveInfinity;
+double B = Double.PositiveInfinity;
 if (B * B == A)
 {
     Console.WriteLine($"{A} is a square of {B}");
---
+++ Ex_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Clear();
+int N = new Random().Next(1, 100);
+int P = new Random().Next(1, 100);
+Console.WriteLine($ "{N}, {P}");
+int remainder = N % P;
+if (remainder == 0)
+{
+    Console.WriteLine($"the number {N} is a multiple of the number {P}");
+}
+else
+{
+    Console.WriteLine($"Result = {remainder}");
+}
