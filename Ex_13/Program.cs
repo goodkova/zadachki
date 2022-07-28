31f3d0d226d6274b4bd09e0ea5964f7c2e4ea939
o.gudkova <oegshop@gmail.com>
2022-07-28 14:31:18 +0400

    Ex_15_02

--- Ex_13/Program.cs
+++ Ex_13/Program.cs
@@ -1,9 +1,33 @@
 ﻿// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
 
+// Console.Clear();
+// Random rnd = new Random();
+// int N = 0;
+// int P = 1;
+// while (N < P)
+// {
+//     N = rnd.Next(1,10);
+//     P = rnd.Next(1,10);
+// }
+// Console.WriteLine($"{N}, {P}");
+// int remainder = N % P;
+// if (remainder == 0)
+// {
+//     Console.WriteLine($"the number {N} is a multiple of the number {P}");
+// }
+// else
+// {
+//     Console.WriteLine($"Result = {remainder}");
+// }
 Console.Clear();
-int N = new Random().Next(1, 100);
-int P = new Random().Next(1, 100);
-Console.WriteLine($ "{N}, {P}");
+int N = new Random().Next(1, 20);
+int P = new Random().Next(1, 20);
+Console.WriteLine($"{N}, {P}");
+if (N < P)
+{
+    N = N + P; P = N - P; N = N - P;
+}
+Console.WriteLine($"{N}, {P}");
 int remainder = N % P;
 if (remainder == 0)
 {
@@ -13,3 +37,4 @@ else
 {
     Console.WriteLine($"Result = {remainder}");
 }
+
---
+++ Ex_15/Program.cs
@@ -0,0 +1,16 @@
+﻿// 15 Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Clear();
+int N = new Random().Next(1, 3500);
+// int N = 0;
+int P1 = 7;
+int P2 = 23;
+Console.WriteLine($"{N}, {P1}, {P2}");
+if (N != 0 && N % P1 == 0 && N % P2 == 0)
+{
+    Console.WriteLine($"the number {N} is a multiple of the number {P1} and {P2}");
+}
+else
+{
+    Console.WriteLine($"Result = not a multiple of the number {P1} and {P2} ");
+}
