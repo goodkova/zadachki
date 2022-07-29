ebdb254a70abad231f4267d6ea6b46a1c89ba9fb
o.gudkova <oegshop@gmail.com>
2022-07-29 21:07:41 +0400

    Ex_28

---
+++ Ex_28/Program.cs
@@ -0,0 +1,31 @@
+﻿// 28. Подсчитать сумму цифр в числе
+
+Console.Clear();
+Console.WriteLine("Enter the number: ");
+
+string str = Console.ReadLine();
+bool flag = true;
+int i = 0;
+int summ = 0;
+
+while (i < str.Length && flag)
+{
+    if (str[i] >= '0' && str[i] <= '9')
+    {
+        // summ += str[i] - '0';
+        i++;
+    }
+    else
+    {
+        flag = false;
+        break;
+    }
+}
+if (flag)
+{
+    Console.WriteLine($"summ = {summ}");
+}
+else
+{
+    Console.WriteLine("Error: Incorrect input");
+}
