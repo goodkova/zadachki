72fdfdef87a881f661d37a88ffb261b04cb4dd05
o.gudkova <oegshop@gmail.com>
2022-07-29 18:28:27 +0400

    Ex_26

---
+++ Ex_26/Program.cs
@@ -0,0 +1,12 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+int A = new Random().Next(1,5);
+int B = new Random().Next(1,5);
+Console.WriteLine($"{A}, {B}");
+int result = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    result *= A;
+}
+Console.WriteLine($"result = {result}");
