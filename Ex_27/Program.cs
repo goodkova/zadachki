4600da442d2a52646d57aa53efa4399be1ec1ffc
o.gudkova <oegshop@gmail.com>
2022-07-29 21:32:55 +0400

    Ex_27

---
+++ Ex_27/Program.cs
@@ -0,0 +1,12 @@
+﻿// 27. Определить количество цифр в числе
+
+
+int num = new Random().Next(1, 10000000);
+Console.WriteLine($"number = {num}");
+int i = 0;
+while (num > 0)
+{
+    num /= 10;
+    i++;
+}
+Console.WriteLine("Number of digits of the entered number: {0}", i);
